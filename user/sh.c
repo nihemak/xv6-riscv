cdf384f121473b84eb95713e9e97e8e3a9cbf78d
Makoto Nihei <nihemak@gmail.com>
2021-08-29 14:32:29 +0900

    refactor

--- user/sh.c
+++ user/sh.c
@@ -127,8 +127,7 @@ int getcmd(char *buf, int nbuf) {
   fprintf(2, "$ ");
   memset(buf, 0, nbuf);
   gets(buf, nbuf);
-  if (buf[0] == 0)  // EOF
-    return -1;
+  if (buf[0] == 0) return -1;  // EOF
   return 0;
 }
 
