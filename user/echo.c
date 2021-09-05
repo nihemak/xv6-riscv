a1df8718c6b8b58ef285ba0a41962b53f815e6d4
Makoto Nihei <nihemak@gmail.com>
2021-09-05 15:36:12 +0900

    refactor

--- user/echo.c
+++ user/echo.c
@@ -3,15 +3,9 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]) {
-  int i;
-
-  for (i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     write(1, argv[i], strlen(argv[i]));
-    if (i + 1 < argc) {
-      write(1, " ", 1);
-    } else {
-      write(1, "\n", 1);
-    }
+    write(1, i + 1 < argc ? " " : "\n", 1);
   }
   exit(0);
 }
