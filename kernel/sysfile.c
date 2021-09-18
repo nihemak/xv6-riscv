b58a359c207c867dc914c2754d41af81386e5db3
Makoto Nihei <nihemak@gmail.com>
2021-09-18 13:04:33 +0900

    refactor

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -32,10 +32,9 @@ static int argfd(int n, int *pfd, struct file **pf) {
 // Allocate a file descriptor for the given file.
 // Takes over file reference from caller on success.
 static int fdalloc(struct file *f) {
-  int fd;
   struct proc *p = myproc();
 
-  for (fd = 0; fd < NOFILE; fd++) {
+  for (int fd = 0; fd < NOFILE; fd++) {
     if (p->ofile[fd] == 0) {
       p->ofile[fd] = f;
       return fd;
@@ -139,10 +138,9 @@ bad:
 
 // Is the directory dp empty except for "." and ".." ?
 static int isdirempty(struct inode *dp) {
-  int off;
   struct dirent de;
 
-  for (off = 2 * sizeof(de); off < dp->size; off += sizeof(de)) {
+  for (int off = 2 * sizeof(de); off < dp->size; off += sizeof(de)) {
     if (readi(dp, 0, (uint64)&de, off, sizeof(de)) != sizeof(de))
       panic("isdirempty: readi");
     if (de.inum != 0) return 0;
@@ -361,20 +359,14 @@ uint64 sys_chdir(void) {
 
 uint64 sys_exec(void) {
   char path[MAXPATH], *argv[MAXARG];
-  int i;
   uint64 uargv, uarg;
 
-  if (argstr(0, path, MAXPATH) < 0 || argaddr(1, &uargv) < 0) {
-    return -1;
-  }
+  if (argstr(0, path, MAXPATH) < 0 || argaddr(1, &uargv) < 0) return -1;
+
   memset(argv, 0, sizeof(argv));
-  for (i = 0;; i++) {
-    if (i >= NELEM(argv)) {
-      goto bad;
-    }
-    if (fetchaddr(uargv + sizeof(uint64) * i, (uint64 *)&uarg) < 0) {
-      goto bad;
-    }
+  for (int i = 0;; i++) {
+    if (i >= NELEM(argv)) goto bad;
+    if (fetchaddr(uargv + sizeof(uint64) * i, (uint64 *)&uarg) < 0) goto bad;
     if (uarg == 0) {
       argv[i] = 0;
       break;
@@ -386,24 +378,23 @@ uint64 sys_exec(void) {
 
   int ret = exec(path, argv);
 
-  for (i = 0; i < NELEM(argv) && argv[i] != 0; i++) kfree(argv[i]);
+  for (int i = 0; i < NELEM(argv) && argv[i] != 0; i++) kfree(argv[i]);
 
   return ret;
 
 bad:
-  for (i = 0; i < NELEM(argv) && argv[i] != 0; i++) kfree(argv[i]);
+  for (int i = 0; i < NELEM(argv) && argv[i] != 0; i++) kfree(argv[i]);
   return -1;
 }
 
 uint64 sys_pipe(void) {
   uint64 fdarray;  // user pointer to array of two integers
   struct file *rf, *wf;
-  int fd0, fd1;
+  int fd0 = -1, fd1;
   struct proc *p = myproc();
 
   if (argaddr(0, &fdarray) < 0) return -1;
   if (pipealloc(&rf, &wf) < 0) return -1;
-  fd0 = -1;
   if ((fd0 = fdalloc(rf)) < 0 || (fd1 = fdalloc(wf)) < 0) {
     if (fd0 >= 0) p->ofile[fd0] = 0;
     fileclose(rf);
@@ -413,8 +404,7 @@ uint64 sys_pipe(void) {
   if (copyout(p->pagetable, fdarray, (char *)&fd0, sizeof(fd0)) < 0 ||
       copyout(p->pagetable, fdarray + sizeof(fd0), (char *)&fd1, sizeof(fd1)) <
           0) {
-    p->ofile[fd0] = 0;
-    p->ofile[fd1] = 0;
+    p->ofile[fd0] = p->ofile[fd1] = 0;
     fileclose(rf);
     fileclose(wf);
     return -1;
