f8c6450086df5d1714a45db81e0879fbba9647b6
Makoto Nihei <nihemak@gmail.com>
2021-09-20 13:20:35 +0900

    refactor

--- user/wc.c
+++ user/wc.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 
+#include "kernel/fcntl.h"
 #include "kernel/stat.h"
 #include "kernel/types.h"
 #include "user/user.h"
@@ -10,7 +11,11 @@ void wc(int fd, char *name) {
   bool inword = false;
   char buf[512];
 
-  while ((n = read(fd, buf, sizeof(buf))) > 0) {
+  while ((n = read(fd, buf, sizeof(buf))) != 0) {
+    if (n < 0) {
+      printf("wc: read error\n");
+      exit(1);
+    }
     for (int i = 0; i < n; i++) {
       c++;
       if (buf[i] == '\n') l++;
@@ -22,21 +27,17 @@ void wc(int fd, char *name) {
       }
     }
   }
-  if (n < 0) {
-    printf("wc: read error\n");
-    exit(1);
-  }
   printf("%d %d %d %s\n", l, w, c, name);
 }
 
 int main(int argc, char *argv[]) {
   if (argc <= 1) {
-    wc(0, "");
+    wc(0 /* stdin */, "");
     exit(0);
   }
 
   for (int i = 1; i < argc; i++) {
-    int fd = open(argv[i], 0);
+    int fd = open(argv[i], O_RDONLY);
     if (fd < 0) {
       printf("wc: cannot open %s\n", argv[i]);
       exit(1);
