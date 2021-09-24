cd745cac78681e13aa75158bd152f61872587d1c
Makoto Nihei <nihemak@gmail.com>
2021-09-24 23:42:46 +0900

    refactor

--- user/init.c
+++ user/init.c
@@ -11,24 +11,19 @@
 
 char *argv[] = {"sh", 0};
 
-int main(void) {
-  int pid, wpid;
+void init_stdout_stderr(void);
 
-  if (open("console", O_RDWR) < 0) {
-    mknod("console", CONSOLE, 0);
-    open("console", O_RDWR);
-  }
-  dup(0);  // stdout
-  dup(0);  // stderr
+int main(void) {
+  init_stdout_stderr();
 
   for (;;) {
     printf("init: starting sh\n");
-    pid = fork();
-    if (pid < 0) {
+    int shell_pid = fork();
+    if (shell_pid < 0) {
       printf("init: fork failed\n");
       exit(1);
     }
-    if (pid == 0) {
+    if (shell_pid == 0) {  // child is shell
       exec("sh", argv);
       printf("init: exec sh failed\n");
       exit(1);
@@ -37,13 +32,22 @@ int main(void) {
     for (;;) {
       // this call to wait() returns if the shell exits,
       // or if a parentless process exits.
-      wpid = wait((int *)0);
-      if (wpid == pid) break;  // the shell exited; restart it.
-      if (wpid < 0) {
+      int wait_pid = wait((int *)0);
+      if (wait_pid == shell_pid) break;  // the shell exited; restart it.
+      if (wait_pid < 0) {
         printf("init: wait returned an error\n");
         exit(1);
       }
       // it was a parentless process; do nothing.
     }
   }
+}
+
+void init_stdout_stderr(void) {
+  if (open("console", O_RDWR) < 0) {
+    mknod("console", CONSOLE, 0);
+    open("console", O_RDWR);
+  }
+  dup(0);  // stdout
+  dup(0);  // stderr
 }
